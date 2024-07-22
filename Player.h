29fefcf800a9b760c6bdd0a01117eaaf4ae9d17a
Isaiah Ameho <zaiya4002@gmail.com>
2024-07-22 11:31:12 -0700

    Update Player.h

---
+++ Player.h
@@ -0,0 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+#include <vector>
+#include <string>
+using namespace std;
+
+
+// work on tug of war
+    class Player{
+
+        public:
+            Player(string name);
+            string _name;
+            int _score
+            vector <string> _items;    
+            
+    }
+
+
+
+
+
+
+#endif
