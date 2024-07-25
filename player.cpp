1df4f2aea1a9009d66938b0f500def8796ef128a
Jorge <mejiapity404@gmail.com>
2024-07-25 08:14:41 -0700

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,11 @@
+#include "player.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std; 
+
+Player::Player() : name(""), score(0) {}
+
+Player::Player(const string& name, int score, const std::string& playerItems)
+
+Player::~Player() {}
