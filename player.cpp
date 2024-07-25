b2ce06dc986c1d4fdd26264b0cbb396f9f72d888
Jorge <mejiapity404@gmail.com>
2024-07-25 08:23:32 -0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -7,5 +7,14 @@ using namespace std;
 Player::Player() : name(""), score(0) {}
 
 Player::Player(const string& name, int score, const std::string& playerItems)
+       : name(name), score(score) {};
 
 Player::~Player() {}
+
+string Player::getName() const { return name; }
+int Player::getScore() const { return score; }
+//vector<vector<char>> Player::getPlayerItems() const { return playerItems; }
+
+void Player::setName(const string& name) { this->name = name; }
+void Player::setScore(int score) { this->score = score; }
+//void Player::setDimension(const vector<vector<char>> playerItems) { this->playerItems = playerItems; }
