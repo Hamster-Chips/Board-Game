7865e4da7158c04fe786cee2bf5a434e7a419aa7
Jorge <mejiapity404@gmail.com>
2024-07-25 11:14:04 -0700

    Update player.cpp

--- Player.h
+++ Player.h
@@ -17,19 +17,19 @@
 
         public:
             Player();
-            Player(const std::string& name, int score, const std::vector<string>& playerItems);
+            Player(const std::string& name, int score, const std::vector<std::string>& playerItems);
             ~Player();
 
             std::string getName() const;
             int getScore() const;
-            std::vector<string> getPlayerItems() const;
+            std::vector<std::string> getPlayerItems() const;
 
             void setName(const std::string& name);
             void setScore(int score);
             void setPlayerItems(const std::vector<std::string>& items);
             void add_items(std::string& playerItems);
             void remove_items(std::string& playerItems);
-            void print_items(const std::vector<string> playerItems);
+            //void print_items(const std::vector<string> playerItems);
       
     };
 
--- player.cpp
+++ player.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 Player::Player() : name(""), score(0), playerItems() {}
 
-Player::Player(const string& name, int score, const std::vector<std::string>& playerItems)
+Player::Player(const string& name, int score, const vector<string>& playerItems)
        : name(name), score(score), playerItems(playerItems) {};
 
 Player::~Player() {}
@@ -17,5 +17,27 @@ vector<string> Player::getPlayerItems() const { return playerItems; }
 
 void Player::setName(const string& name) { this->name = name; }
 void Player::setScore(int score) { this->score = score; }
-void Player::setPlayerItems(const vector<string>& items) { playerItems = playerItems;}
+void Player::setPlayerItems(const vector<string>& items) { this-> playerItems = items;}
 
+
+int main()
+{
+    vector<string> lays ({"BOO", "hi"});
+    Player player1;
+    player1.setName("Jorge");
+    player1.setScore(0);
+    player1.setPlayerItems(lays);
+
+    cout << "player Name: " << player1.getName() << endl;
+    cout << "player score: " << player1.getScore() << endl;
+    cout << "player items: ";
+    for (const string items: player1.getPlayerItems()){
+        cout<< items << " ";
+    }
+
+
+
+
+    return 0; 
+
+}
