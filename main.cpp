e76794854a2953635a085d7c4bbd5ada67108988
Classy-Brian <phanbrian101@gmail.com>
2024-07-28 13:34:43 -0700

    main.cpp - worked o setMap

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <fstream>
 #include "map.h"
 #include "enemy.h"
 
@@ -31,7 +32,39 @@ int main()
 
 }
 
-void setMap(const string& name, int mapNum, const string& revealMap)
+void setMap(const string& name, int mapNum, const string& pathMap)
 {
     Map myMap(name, 3, "map3.txt");
+
+    ifstream file(pathMap);
+    if (!file.is_open())
+    {
+        cerr << "Failed to open file: " << pathMap << endl;
+        return;
+    }
+
+    string line;
+    vector<vector<char>> tempMap;
+    vector<Enemy> enemies;
+    vector<string> rewards = {"gold", "item"};
+    int x = 0;
+    int y = 0;
+    while (getline(file, line))
+    {
+        vector<char> row;
+        for (char c : line)
+        {
+            if (c == '@')
+            {
+                Enemy newEnemy("wack", 10, rewards);
+                enemies.push_back(newEnemy);
+            }
+            x += 1;
+        }
+        y += 1;
+        x = 0;
+    }
+
+    // Maybe create a struct to hold all info of the map or store it in the enemy and/or map class
+    
 }
