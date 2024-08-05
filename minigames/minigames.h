b090960243a8c3c7e7f6414f21a0b7544232e5a1
Jorge <mejiapity404@gmail.com>
2024-08-05 11:31:19 -0700

    Update minigames.cpp/minigames.h

--- minigames/minigames.h
+++ minigames/minigames.h
@@ -10,7 +10,8 @@
 
         private:
 
-            std::string name; 
+            std::string name;
+            std::string description;
             std::vector<int> playersScore;
             std::vector<std::string> players;
             std::vector<int> reward;
@@ -19,21 +20,23 @@
         public:
         
             Minigames();
-            Minigames(const std::string& name, const std::vector<int>& playersScore, const std::vector<std::string>& players, const std::vector<int>& reward);
+            Minigames(const std::string& name, const std::string& description, const std::vector<int>& playersScore, const std::vector<std::string>& players, const std::vector<int>& reward);
             ~Minigames();
 
             std::string getName() const;
+            std::string getDescription() const;
             std::vector<int> getPlayersScore() const;
             std::vector<std::string> getPlayers() const;
             std::vector<int> getReward() const;
 
             void setName(const std::string& name);
+            void setDescription(const std::string& description);
             void setPlayersScore(const std::vector<int>& playersScore);
             void setPlayers(const std::vector<std::string>& players);
             void setReward(const std::vector<int>& reward);
 
-            void printMinigames() const;
-            void printResult() const; 
+            void printMinigames(const std::string& name, const std::string& description);
+            void printResult(const std::vector<int>& playerscore, std::vector<std::string>& players); 
     };
 
 #endif
