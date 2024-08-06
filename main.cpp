071a7cb92a27d102ffea1c0553d09fcad4200a87
Classy-Brian <phanbrian101@gmail.com>
2024-08-06 10:16:30 -0700

    Clean up code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream> 
 #include <fstream>
+
 #include "map.h"
 #include "enemy.h"
+#include "player.h"
 
 using namespace std;
 
 /*
     Notes for myself:
-    To run put in the cmd like: g++ -o main.exe map.h map.cpp enemy.h enemy.cpp main.cpp (enter) ./main.exe
+    To run put in the cmd like: g++ -o main.exe map.h map.cpp enemy.h enemy.cpp main.cpp player.h player.cpp (enter) ./main.exe
 */
 
 vector<Enemy> makeEnemy(const string& revealMap);
@@ -16,6 +18,7 @@ int main()
 {
     cout << "Hello World!\n" << endl;
 
+    // Testing map
     Map myMap("Le map", 3, "map3.txt");
     Map revealMap("NO ONE SEE", 3, "reveal3.txt");
     vector<Enemy> enemies = makeEnemy("reveal3.txt");
@@ -26,9 +29,24 @@ int main()
     revealMap.printMap();
     cout << endl;
 
+    // Testing enemy
     for (Enemy enemy : enemies)
         enemy.printEnemy();
     cout << endl;
+
+    // Testing player
+    vector<string> lays ({"BOO", "hi"});
+    Player player1;
+    player1.setName("Jorge");
+    player1.setScore(0);
+    player1.setPlayerItems(lays);
+    player1.addItems("doo");
+    player1.removeItems("hi");
+    player1.printItems();
+
+    cout << "player Name: " << player1.getName() << endl;
+    cout << "player score: " << player1.getScore() << endl;
+    cout << "player items: ";
     
 }
 
--- player.h
+++ player.h
@@ -1,38 +1,34 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-
 #include <vector>
 #include <string>
 
+class Player
+{
+private:
+    std::string name; 
+    int score;
+    std::vector<std::string> playerItems; 
 
-// player
-    class Player{
+public:
+    Player();
+    Player(const std::string& name, int score, const std::vector<std::string>& playerItems);
+    ~Player();
 
-        private:
-            std::string name; 
-            int score;
-            std::vector<std::string> playerItems; 
+    std::string getName() const;
+    int getScore() const;
+    std::vector<std::string> getPlayerItems() const;
 
+    void setName(const std::string& name);
+    void setScore(int score);
+    void setPlayerItems(const std::vector<std::string>& items);
 
-        public:
-        
-            Player();
-            Player(const std::string& name, int score, const std::vector<std::string>& playerItems);
-            ~Player();
-
-            std::string getName() const;
-            int getScore() const;
-            std::vector<std::string> getPlayerItems() const;
-
-            void setName(const std::string& name);
-            void setScore(int score);
-            void setPlayerItems(const std::vector<std::string>& items);
-
-            void addItems(const std::string& playerItems);
-            void removeItems(const std::string& items);
-            void printItems() const;
-      
-    };
+    void addItems(const std::string& playerItems);
+    void removeItems(const std::string& items);
+    
+    void printItems() const;
+    
+};
 
 #endif
