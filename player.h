eff91bd2a16c4320f36ba278479568c91e813258
Isaiah Ameho <zaiya4002@gmail.com>
2024-08-08 10:26:49 -0700

    For real

---
+++ player.h
@@ -0,0 +1,47 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+#include <vector>
+#include <string>
+
+
+// player
+    class Player{
+
+        private:
+            std::string name; 
+            int score;
+            std::vector<std::string> playerItems; 
+            std::vector<int> playerLoc;
+            
+
+
+        public:
+            Player();
+            Player(const std::string& name, int score, const std::vector<std::string>& playerItems);
+            ~Player();
+
+            std::string getName() const;
+            int getScore() const;
+            std::vector<std::string> getPlayerItems() const;
+            
+            
+
+
+            void setName(const std::string& name);
+            void setScore(int score);
+            void setPlayerItems(const std::vector<std::string>& items);
+            void add_items(const std::string& playerItems);
+            void remove_items(const std::string& items);
+            void print_items() const;
+
+
+            std::vector<int> getPlayerLoc();
+            void setPlayerLoc(std::vector<int> newLoc);
+            
+            
+
+    };
+
+#endif
