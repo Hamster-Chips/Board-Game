95386cf25bc862ebe5703845918077c18c85fd91
Jorge <mejiapity404@gmail.com>
2024-08-12 10:11:46 -0700

    Update minigames.cpp

--- minigames/minigames.cpp
+++ minigames/minigames.cpp
@@ -25,6 +25,23 @@ void Minigames::setPlayersScore(const vector<int>& playersScore) { this->players
 void Minigames::setPlayers(const vector<Player>& players) { this->players = players;}
 void Minigames::setReward(const vector<int>& reward) { this->reward = reward;}
 
+void myswap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(vector<int>& playersScore)
+{
+    for(int i = 0; i < playersScore.size() - 1 ; i++){
+        for(int j = 0; j< playersScore.size() - 1 - i; j++){
+            if(playersScore[j] > playersScore[j+1]){
+                myswap(&playersScore[j],&playersScore[j+1]);
+            }
+        }
+    }
+}
+
 void Minigames::printMinigames(const string& name, const string& description){
     cout << "Welcome to " << name << "\n";
     cout << "How to play: " << description << "\n";
@@ -33,26 +50,38 @@ void Minigames::printMinigames(const string& name, const string& description){
 void Minigames::printResult(const vector<int>& playersScore, vector<Player>& players){
     cout <<  "Results: \n" << endl;
     int num = 1;
-    int num2 =1;
-    bool over = false; 
-    while (!over)
-    {
-        for(int i = 0; i < players.size();i++){
-            cout << num << ". " << players[i].getName() << " " << players[i].getScore() << " +"<< playersScore[i] << "\n" << endl;
-            num += 1;
-            players[i].setScore(players[i].getScore() + playersScore[i]);
-            // player get new score by set 
-        }
-        pause();
-
-        cout << " Final Results: \n " << endl;
-        for(int i = 0; i < players.size();i++){
-            cout << num2 << ". " << players[i].getName() << " " << players[i].getScore() << "\n" << endl;
-            num2 += 1;
-            //pause
-        }
-        over = true; 
+   
+    for(int i = 0; i < players.size();i++){
+        cout << num << ". " << players[i].getName() << " " << players[i].getScore() << " +"<< playersScore[i] << "\n" << endl;
+        players[i].setScore(players[i].getScore() + playersScore[i]);
+        num ++;
+        // player get new score by set 
     }
+
+    vector<pair<int, Player>> playerScorePairs;
+    for (size_t i = 0; i < players.size(); i++) {
+        playerScorePairs.push_back(make_pair(players[i].getScore(), players[i]));
+    }
+
+    // Sort players based on scores (descending order)
+    sort(playerScorePairs.begin(), playerScorePairs.end(), [](const pair<int, Player>& a, const pair<int, Player>& b) {
+        return a.first > b.first;
+    });
+
+    // Update players vector with sorted players
+    for (size_t i = 0; i < players.size(); i++) {
+        players[i] = playerScorePairs[i].second;
+    }
+
+    pause();
+
+    cout << " Final Results: \n " << endl;
+    int num2 = 1;
+    for(int i = 0; i < players.size();i++){
+        cout << num2 << ". " << players[i].getName() << " " << players[i].getScore() << "\n" << endl;
+        num2 += 1;
+    }
+    pause();
 }
 
 
